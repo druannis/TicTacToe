31f9e7a997a99d9e3521fdacfd5ec485219712ff
Tatiana Gallivan <ttnana882@gmail.com>
2023-10-06 21:10:52 -0400

    Move methods to controller class, connect controller class and main class

--- app/src/main/java/com/zybooks/tictactoe/MainActivity.java
+++ app/src/main/java/com/zybooks/tictactoe/MainActivity.java
@@ -1,34 +1,28 @@
 package com.zybooks.tictactoe;
 
-import androidx.appcompat.app.AppCompatActivity;
+//  Tatiana Gallivan and Dru Annis
+//  ISYS 221 - VL1
+//  TicTacToe
+//  Due: 10/8/2023
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+//Imports
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
 import androidx.appcompat.widget.Toolbar;
 
+
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    //Controller sends data from view to model.
+
     Toolbar mToolbar;
+    private TicController controller; // Makes the TicController class the controller
+    public Button[][] buttons = new Button[3][3];
 
-    public static Button[][] buttons = new Button[3][3];
-
-    private boolean player1 = true;
-
-    private int roundNumber = 0;
-    private boolean gameWon = false;
-
-
-
-    private TextView textViewPlayer1;
-    private TextView textViewPlayer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,109 +40,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         }
+        controller = new TicController(this, buttons); // Used to connect the buttons to the controller class
+
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) { // This creates the toolbar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.toolbarbuttons, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
-
     @Override
-    public void onClick(View view) {
-        if (!((Button) view).getText().toString().equals("") || gameWon) {
-            return;
-        }
-
-        if (player1) {
-            ((Button) view).setText("X");
-        } else {
-            ((Button) view).setText("O");
-        }
-
-        roundNumber++;
-        player1 = !player1;
-
-        // Check for a winner or tie
-        if (checkForWin() || roundNumber == 9) {
-            endGame();
-        }
-    }
-
-    private boolean checkForWin() {
-        // Check rows for a win
-        for (int i = 0; i < 3; i++) {
-            if (checkRowCol(buttons[i][0], buttons[i][1], buttons[i][2])) {
-                return true;
-            }
-        }
-
-        // Check columns for a win
-        for (int i = 0; i < 3; i++) {
-            if (checkRowCol(buttons[0][i], buttons[1][i], buttons[2][i])) {
-                return true;
-            }
-        }
-
-        // Check diagonals for a win
-        if (checkRowCol(buttons[0][0], buttons[1][1], buttons[2][2]) ||
-                checkRowCol(buttons[0][2], buttons[1][1], buttons[2][0])) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.newGame) {
+            controller.confirmNewGameDialog();  // Ask user if they are sure they want to start a new game
             return true;
-        }
-
-        return false;
-    }
-
-    private boolean checkRowCol(Button b1, Button b2, Button b3) {
-        // Check if the three buttons have the same non-empty text (X or O)
-        String text1 = b1.getText().toString();
-        String text2 = b2.getText().toString();
-        String text3 = b3.getText().toString();
-        return !text1.isEmpty() && text1.equals(text2) && text1.equals(text3);
-    }
-
-    private void endGame() {
-        gameWon = true;
-        // Disable all buttons to prevent further moves
-        for (int i = 0; i < 3; i++) {
-            for (int k = 0; k < 3; k++) {
-                buttons[i][k].setEnabled(false);
-            }
-        }
-
-        // Display a message indicating the winner or a tie
-        if (checkForWin()) {
-            String winnerText = player1 ? "Player O wins!" : "Player X wins!";
-            showResultDialog(winnerText);
+        } else if (itemId == R.id.quitGame) {
+            finish(); // Closes the app
+            return true;
         } else {
-            showResultDialog("CAT wins!");
+            return super.onOptionsItemSelected(item);
         }
     }
 
-    private void showResultDialog(String winnerText) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(winnerText)
-                .setPositiveButton("New Game", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // Implement logic to start a new game here
-                        startNewGame();
-                    }
-                })
-                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // Implement logic to exit the game here
-                        finish();
-                    }
-                });
-        builder.show();
+    @Override // This method listen to see if users click buttons
+    public void onClick(View view) {
+        if (view instanceof Button) {
+            controller.onButtonClick((Button) view);
+        }
     }
-
-    private void startNewGame() {
-        // Implement logic to start a new game here
-        // Reset game variables and board
-    }
-
 }
+
--- app/src/main/java/com/zybooks/tictactoe/TicController.java
+++ app/src/main/java/com/zybooks/tictactoe/TicController.java
@@ -1,51 +1,128 @@
 package com.zybooks.tictactoe;
 
 
-import android.os.Bundle;
-import android.view.View;
+import android.app.AlertDialog;
 import android.widget.Button;
-import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class TicController {
 
-public class TicController extends AppCompatActivity{
-//Model Does Logic
-private boolean checkWin() {
-    String[][] field = new String[3][3];
+    private final MainActivity view;
+    private Button[][] buttons;
 
-    for (int i = 0; i < 3; i++) {
-        for (int k = 0; k < 3; k++) {
-            field[i][k] = MainActivity.buttons[i][k].getText().toString();
+    public TicController(MainActivity view, Button[][] buttons) { //connects controller to the view
+        this.view = view;
+        this.buttons = buttons;
+    }
+    private boolean player1 = true;
+    private int roundNumber = 0;
+    private boolean gameWon = false;
+
+
+    public void onButtonClick(Button button) {
+        if (!button.getText().toString().equals("") || gameWon) {
+            return;
+        }
+
+        if (player1) {
+            button.setText("X");
+        } else {
+            button.setText("O");
+        }
+
+        roundNumber++;
+        player1 = !player1;
+
+        // Check for a winner or tie
+        if (checkForWin() || roundNumber == 9) {
+            endGame();
         }
     }
+
+
+    private boolean checkForWin() {
+    // Check rows for a win
     for (int i = 0; i < 3; i++) {
-        if (field[i][0].equals(field[i][1])
-                && field[i][0].equals(field[i][2])
-                && !field[i][0].equals("")) {
+        if (checkRowCol(buttons[i][0], buttons[i][1], buttons[i][2])) {
             return true;
         }
     }
+
+    // Check columns for a win
     for (int i = 0; i < 3; i++) {
-        if (field[0][i].equals(field[1][i])
-                && field[0][i].equals(field[2][i])
-                && !field[0][i].equals("")) {
+        if (checkRowCol(buttons[0][i], buttons[1][i], buttons[2][i])) {
             return true;
         }
     }
-    if (field[0][0].equals(field[1][1])
-            && field[0][0].equals(field[2][2])
-            && !field[0][0].equals("")) {
-        return true;
-    }
 
-    if (field[0][2].equals(field[1][1])
-            && field[0][2].equals(field[2][0])
-            && !field[0][2].equals("")) {
-        return true;
-    }
-
-    return false;
-
+    // Check diagonals for a win
+    return checkRowCol(buttons[0][0], buttons[1][1], buttons[2][2]) ||
+            checkRowCol(buttons[0][2], buttons[1][1], buttons[2][0]);
 }
+
+
+    private boolean checkRowCol(Button b1, Button b2, Button b3) {
+        // Check if the three buttons have symbol (X or O)
+        String text1 = b1.getText().toString();
+        String text2 = b2.getText().toString();
+        String text3 = b3.getText().toString();
+        return !text1.isEmpty() && text1.equals(text2) && text1.equals(text3);
+    }
+
+    private void endGame() {
+        gameWon = true;
+        // Disable all buttons to prevent more moves
+        for (int i = 0; i < 3; i++) {
+            for (int k = 0; k < 3; k++) {
+                buttons[i][k].setEnabled(false);
+            }
+        }
+
+        // Display a message showing the winner or a tie
+        if (checkForWin()) {
+            String winnerText = player1 ? "Player O wins!" : "Player X wins!";
+            showResultDialog(winnerText);
+        } else {
+            showResultDialog("CAT wins!");
+        }
+    }
+    private void showResultDialog(String winnerText) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(view);
+        builder.setMessage(winnerText)
+                .setPositiveButton("OK", (dialog, id) -> {
+                    // Close dialog when OK is clicked
+                    dialog.dismiss();
+                });
+        builder.show();
+    }
+        void confirmNewGameDialog() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(view);
+            builder.setMessage("Are you sure you want to start a new game?")
+                    .setPositiveButton("Yes", (dialog, id) -> {
+                        // User confirmed, start a new game
+                        startNewGame();
+                    })
+                    .setNegativeButton("No", (dialog, id) -> {
+                        // User canceled, do nothing
+                    });
+        builder.show();
+    }
+
+
+    private void startNewGame() {
+        // Reset variables
+        player1 = true;
+        roundNumber = 0;
+        gameWon = false;
+
+        // Clear the board
+        for (int i = 0; i < 3; i++) {
+            for (int k = 0; k < 3; k++) {
+                buttons[i][k].setText("");
+                buttons[i][k].setEnabled(true);
+            }
+        }
+        }
+
+
 }
 
